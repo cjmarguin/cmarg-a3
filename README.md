23b4e0846eee2bff66ad716e7e0de35678e8544c
Claudia "CJ" Marguin <marguin_claudia@columbusstate.edu>
2019-12-03 21:07:16 +0000

    oop in minor

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # cmarg-a3
 Claudia's 3rd SS Assignment
+
+
+
+
+# Reminders for Final
+
+npm install (package) --save
+
+init
+express
+node-fetch
+body-parser
+
+package.json >> "start": "node app.js"
+Create Procfile  >> web: npm start
+
+app.js
+
+index.ejs >> views
+
+styles >> public
+
--- app.js
+++ app.js
@@ -8,33 +8,22 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ encoded: true}));
-var task = ["clean", "cook"];
-var complete = ["eat", "sleep"];
+app.use(bodyParser.urlencoded({ extended: true}));
 
 
 app.get('/', function(req, res){
-    res.render('index',{task:task, complete:complete});
-});
-
-app.post('/addtask', function(req, res){
-    var newTask = req.body.newtask;
-    task.push(newTask);
-    res.redirect("/");
-});
-
-app.post('/removetask', function(req, res){
-    var completeTask = req.body.check;
-    if(typeof completeTask === "string"){
-        complete.push(completeTask);
-        task.splice(task.indexOf(completeTask), 1);
-    }else if(typeof completeTask === "object"){
-        for(var i = 0; i < completeTask.length ; i++){
-            complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1);
-        }
-    }
-    res.redirect("/");
+    let url = 'https:https://xkcd.com/info.0.json';
+    let img;
+    nodeFetch(url)
+    .then(resp => resp.json())
+    .then(data =>{
+        img = data.img;
+        res.render('index', {img:img});
+    })
+    .catch(err => {
+        console.log(err);
+        res.render('index');
+    });
 });
 
 http.createServer(app).listen(port, function(){
